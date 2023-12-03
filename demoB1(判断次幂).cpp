bae7b5897485b4799adc534f4f950917cd6e4e85
dontLoveby <142394137+dontLoveby@users.noreply.github.com>
2023-12-03 14:41:04 +0800

    Add files via upload

--- demoB1(判断次幂).cpp
+++ demoB1(判断次幂).cpp
@@ -1,21 +1,11 @@
 // 判断a是否是二的次幂，是返回是二的几次幂，否返回0
-using namespace std;
 #include<iostream>
-int sum;
-long long a;
-long long f(long long a);
-int main()
-{
-	cin >> a;
-	cout << f(a) << endl;
-	return 0;
-}
 
-long long f(long long a = 0) {
-	// 判断a是否是二的次幂，是返回是二的几次幂，否返回0
-	while (a > 1) {
-		if (a >> 1 << 1== a) {
-			a = a >> 1;
+int f(long long arg = 0) {
+	int sum = 0;
+	while (arg > 1) {
+		if (arg >> 1 << 1== arg) {
+			arg = arg >> 1;
 			sum++;
 		} else {
 		 	sum = 0;
@@ -26,4 +16,14 @@ long long f(long long a = 0) {
 		sum = 0;
 	}
 	return sum;
+}
+
+// 示例程序
+long long a;
+long long f(long long arg);
+int main()
+{
+	std::cin >> a;
+	std::cout << f(a) << std::endl;
+	return 0;
 }
